683613ec1ef9e48620bee3f86dc34b6e89d5fe75
alexe23b <alexe23b@gmail.com>
2019-04-27 19:17:31 +0300

    add pattern singleton

---
+++ DesignPatterns/Singleton/SingletonTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DesignPatterns\Singleton;
+
+use DesignPatterns\Singleton;
+use PHPUnit\Framework\TestCase;
+
+class SingletonTest extends TestCase
+{
+    public function testUniqueness()
+    {
+        $firstCall = Singleton::getInstance();
+        $secondCall = Singleton::getInstance();
+
+        $this->assertInstanceOf(Singleton::class, $firstCall);
+        $this->assertSame($firstCall, $secondCall);
+    }
+}
